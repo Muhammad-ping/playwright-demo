b615e2cbad3a4c2856daad52b229549b66560538
Muhammad-ping <sssoliev.m@gmail.com>
2026-03-17 12:45:01 -0400

    new

--- tests/plauwright-locators/getByAltText.spec.ts
+++
@@ -1,9 +0,0 @@
-// Using getByAltText()  Use this for images or elements with alt attributes.
-// Использование getByAltText() Используйте это для изображений или элементов с атрибутом alt.
-
-import { test, expect } from "@playwright/test";
-
-test("getByAlttext", async ({ page }) => {
-  await page.goto("https://selectors-practice.onrender.com/");
-  await page.getByAltText("toggle-switch").click();
-});
